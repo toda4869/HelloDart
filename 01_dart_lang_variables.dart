65e36dbe6aa2ea7e4e2cabc3d864b6bf205e07e9
toda4869 <1950454955@qq.com>
2022-09-20 11:28:05 +0000

    commit

---
+++ 01_dart_lang_variables.dart
@@ -0,0 +1,20 @@
+// 变量 （https://dart.cn/samples#variables）
+void variables() {
+
+  var name = 'Voyager I';
+  var year = 1977;
+  print('varname is ${name},year is ${year}');
+  }
+
+void name_object_string(){
+    Object name='Bob';
+    print('value of object name is $name');
+
+    String name_string = 'Bob';
+    print('value of string name is $name_string');
+}
+
+  void main(){
+    variables();
+    name_object_string();
+  }
